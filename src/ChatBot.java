9a7f2685157047d0e3beb00bcf101d9319ed9b73
SunYoung <kimsuny@mail.gvsu.edu>
2014-04-03 22:52:42 -0400

    Updating ChatBot

--- src/ChatBot.java
+++ src/ChatBot.java
@@ -5,6 +5,8 @@
  * Thomas Sniadecki
  */
 import java.util.*;
+import java.net.*;
+import java.io.*;
 
 public class ChatBot {
 	
@@ -27,8 +29,23 @@ public class ChatBot {
 		return output;
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		
+		// TODO: Find the wiki doc url from user input
+		URL url = new URL("http://en.wikipedia.org");
+		BufferedReader in = new BufferedReader(
+				new InputStreamReader(url.openStream()));
+		
+		String inputLine;
+		while((inputLine = in.readLine()) != null) {
+			
+			// TODO: Tokenize the user input to find the keywords
+			if(inputLine.contains("div"))
+				System.out.println("\t" + inputLine);
+		}
+		in.close();
+		
+	/*
 		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
 		String input;
@@ -39,6 +56,7 @@ public class ChatBot {
 			System.out.print("ChatBot > ");
 			System.out.println(input);
 		}
+	*/
 		
 	}
 }
